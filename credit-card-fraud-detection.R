9494404ed99e4cda73e0e480155e1c4f61c7fbaa
Aniket Kumar <aniketkumar552004@gmail.com>
2025-02-04 20:10:51 +0530

    Initial commit

---
+++ credit-card-fraud-detection.R
@@ -0,0 +1,44 @@
+
+install.packages(c("randomForest", "caret", "ROSE", "ggplot2"), dependencies = TRUE)
+
+
+library(randomForest)
+library(caret)
+library(ROSE)
+library(ggplot2)
+
+
+df <- read.csv("C:/Users/ANIKET KUMAR/OneDrive/Desktop/creditcard.csv")
+
+
+df$Class <- as.factor(df$Class)
+
+
+table(df$Class)
+
+
+df_balanced <- ROSE(Class ~ ., data = df, seed = 42)$data
+table(df_balanced$Class) 
+
+
+set.seed(42)
+trainIndex <- createDataPartition(df_balanced$Class, p = 0.8, list = FALSE)
+trainData <- df_balanced[trainIndex, ]
+testData  <- df_balanced[-trainIndex, ]
+
+
+rf_model <- randomForest(Class ~ ., data = trainData, ntree = 50)
+
+
+rf_pred <- predict(rf_model, testData)
+
+
+conf_matrix <- confusionMatrix(rf_pred, testData$Class)
+print(conf_matrix)
+
+
+ggplot(df, aes(x = Amount, fill = Class)) +
+  geom_histogram(bins = 50, alpha = 0.7, position = "identity") +
+  scale_fill_manual(values = c("blue", "red")) +
+  theme_minimal() +
+  labs(title = "Transaction Amounts Distribution", x = "Transaction Amount", y = "Count")
